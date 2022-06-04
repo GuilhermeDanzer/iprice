406371c40dadda5982a670da53ed038083962725
GuilhermeDanzer <guidanzer@hotmail.com>
2022-06-03 21:25:18 -0300

    Commit

--- WebScrapple/teste.js
+++ WebScrapple/teste.js
@@ -1,163 +1,201 @@
-const puppeteer = require('puppeteer')
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const puppeteer = require("puppeteer")
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
 
 let browsePromise = puppeteer.launch({
-  args:[
+  args: [
     "--disable-gpu",
     "--disable-dev-shm-usage",
     "--no-sandbox",
-    "--disable-setuid-sandbox"
+    "--disable-setuid-sandbox",
   ],
-
-  defaultViewport: {width: 1280,
-    height: 720},
+  headless: false,
+  defaultViewport: { width: 1280, height: 720 },
 })
 const app = express()
 app.use(cors())
 
+exports.foods = app.post("/foods", async (req, res) => {
+  const browser = await browsePromise
+  const context = await browser.createIncognitoBrowserContext()
 
-exports.foods = app.post('/foods',async (req,res)=>{
-  const browser = await browsePromise;
-  const context = await browser.createIncognitoBrowserContext();
- 
-  
-  try{
-    const {endereco,comida} = req.body
-
+  try {
+    const { endereco, comida } = req.body
 
     const page = await context.newPage()
-    page.setDefaultNavigationTimeout(120000);
-    const version = await page.browser().version();
+    page.setDefaultNavigationTimeout(120000)
+    const version = await page.browser().version()
     console.log(version)
-    page.on('error', err=> {
-      console.log('error happen at the page: ', err);
-    });
-  
-    page.on('pageerror', pageerr=> {
-      console.log('pageerror occurred: ', pageerr);
+    page.on("error", err => {
+      console.log("error happen at the page: ", err)
     })
-    await page.goto('https://www.ifood.com.br/lista-restaurantes')
-    await page.$eval("body > div.ReactModalPortal > div > div > div > div > div > div:nth-child(1) > div > div > div.address-list-step__container.address-list-step__container--visible > div.address-search-input.address-search-input--role-button > button.address-search-input__button",button=>button.click())
-    await page.waitFor(2000)
-    await page.evaluate((endereco) => {
+
+    page.on("pageerror", pageerr => {
+      console.log("pageerror occurred: ", pageerr)
+    })
+    await page.goto("https://www.ifood.com.br/lista-restaurantes")
+    await page.$eval(
+      "body > div.ReactModalPortal > div > div > div > div > div > div:nth-child(1) > div > div > div.address-list-step__container.address-list-step__container--visible > div.address-search-input.address-search-input--role-button > button.address-search-input__button",
+      button => button.click()
+    )
+    await page.waitForTimeout(2000)
+    await page.evaluate(endereco => {
       //const message = 'av brasil oeste 888 passo fundo';
-   
+
       const message = endereco
       console.log(endereco)
-      document.execCommand("insertText", false, message);
-    },endereco);
-    console.log('chegou')
-    await page.waitFor(2000)
-    await page.$eval("body > div.ReactModalPortal > div > div > div > div > div > div:nth-child(2) > div > div.address-search-step > div.address-search-step__results > ul > li:nth-child(1) > div > button", teste=>teste.click())
-    await page.$eval("body > div.ReactModalPortal > div > div > div > div > div > div:nth-child(2) > div > div.address-search-step > div.address-search-step__results > ul > li:nth-child(1) > div > button", teste=>teste.click())
-    await page.waitFor(3000)
-    await page.$eval('body > div.ReactModalPortal > div > div > div > div > div > div:nth-child(3) > div.address-maps__map > button > canvas',opa=>opa.click())
-    await page.$eval('body > div.ReactModalPortal > div > div > div > div > div > div:nth-child(3) > div.address-map-step__complete-form.address-map-step__complete-form--active > div.complete-address > form > div.complete-address--save-btn > button > canvas',teste=>teste.click())
-    await page.waitFor(1000)
-    await page.focus('#__next > div > header > div > div.search-input > form > div > input')
-    //page.keyboard.type('cachorro quente')
-    page.keyboard.type(comida)
-    await page.waitFor(1000)
-    page.keyboard.press('Enter')
-    await page.waitFor(2000)
-    await page.$eval('#marmita-tab1-3',aba=>aba.click())
-    const arrayProducts = []
-    await page.waitFor(3000)
-    var tamanhoLista = await page.$eval(
-      '#marmita-tab1-3', divs => divs.innerText
+      document.execCommand("insertText", false, message)
+    }, endereco)
+    console.log("chegou")
+    await page.waitForTimeout(3000)
+
+    await page.$eval(
+      "body > div.ReactModalPortal > div > div > div > div > div > div:nth-child(2) > div > div.address-search-step > div.address-search-step__results > ul > li:nth-child(1) > div > button",
+      teste => teste.click()
+    )
+    await page.waitForTimeout(3000)
+
+    //selecting address
+    await page.$eval(
+      "body > div.ReactModalPortal > div > div > div > div > div > div:nth-child(2) > div > div.address-search-step > div.address-search-step__results > ul > li:nth-child(1) > div > button",
+      teste => teste.click()
+    )
+    //click map button twice
+    await page.waitForTimeout(3000)
+    await page.$eval(
+      "body > div:nth-child(10) > div > div > div > div > div > div:nth-child(3) > div.address-maps__map > button",
+      opa => opa.click()
+    )
+    await page.waitForTimeout(5000)
+    await page.$eval(
+      "body > div:nth-child(10) > div > div > div > div > div > div:nth-child(3) > div.address-finder__complete-form.address-finder__complete-form--active > div.complete-address > form > div.complete-address--save-btn > button",
+      teste => teste.click()
+    )
+    await page.waitForTimeout(2000)
+    await page.focus(
+      "#__next > div:nth-child(1) > header > div.search-input > form > div > input"
     )
 
-    if (tamanhoLista.length === 9){
-      tamanhoLista = tamanhoLista.slice(7,8)
+    //choosing the food
+    //page.keyboard.type('cachorro quente')
+    page.keyboard.type(comida)
+    await page.waitForTimeout(1000)
+    page.keyboard.press("Enter")
+    await page.waitForTimeout(2000)
+    await page.$eval("#marmita-tab1-3", aba => aba.click())
+    const arrayProducts = []
+    await page.waitForTimeout(3000)
+    var tamanhoLista = await page.$eval(
+      "#marmita-tab1-3",
+      divs => divs.innerText
+    )
+
+    if (tamanhoLista.length === 9) {
+      tamanhoLista = tamanhoLista.slice(7, 8)
       tamanhoLista = parseInt(tamanhoLista)
-    }else if(tamanhoLista.length===10){
-      tamanhoLista = tamanhoLista.slice(7,9)
+    } else if (tamanhoLista.length === 10) {
+      tamanhoLista = tamanhoLista.slice(7, 9)
       tamanhoLista = parseInt(tamanhoLista)
-    }else if(tamanhoLista.length===11){
-      tamanhoLista = tamanhoLista.slice(7,10)
+    } else if (tamanhoLista.length === 11) {
+      tamanhoLista = tamanhoLista.slice(7, 10)
       tamanhoLista = parseInt(tamanhoLista)
     }
 
     for (var i = 1; i <= tamanhoLista; i++) {
       const title = await page.$eval(
-        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > div.dish-card__info > div > h3`, divs => divs.innerText
+        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > div.dish-card__info > div > h3`,
+        divs => divs.innerText
       )
       const description = await page.$eval(
-        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > div.dish-card__info > div > span`, divs => divs.innerText
+        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > div.dish-card__info > div > span`,
+        divs => divs.innerText
       )
-      
+
       const link = await page.$eval(
-        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a`, a => a.href
+        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a`,
+        a => a.href
       )
       var price = await page.$eval(
-        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > div.dish-card__info > span.dish-card__price`, divs => divs.innerText
+        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > div.dish-card__info > span.dish-card__price`,
+        divs => divs.innerText
       )
-      price = price.substring(0,8)
-      
-  
-      price = price.replace(/\$/g,'')
-      price = price.replace(/R/g,'')
-  
-      price = parseFloat(price.replace(',','.').replace(' ',''))
-      
-       
+      price = price.substring(0, 8)
+
+      price = price.replace(/\$/g, "")
+      price = price.replace(/R/g, "")
+
+      price = parseFloat(price.replace(",", ".").replace(" ", ""))
+
       const tempo = await page.$eval(
-        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > div.dish-card__info > span.dish-card__delivery > span.dish-card__delivery-time`, divs => divs.innerText
+        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > div.dish-card__info > span.dish-card__delivery > span.dish-card__delivery-time`,
+        divs => divs.innerText
       )
-      
 
       var entrega = await page.$eval(
-        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > div.dish-card__info >  span.dish-card__delivery > span.dish-card__delivery-fee`, divs => divs.innerText
+        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > div.dish-card__info >  span.dish-card__delivery > span.dish-card__delivery-fee`,
+        divs => divs.innerText
       )
       var total = 0
 
-      if (entrega!=='Entrega grátis'){
+      if (entrega !== "Entrega grátis") {
         entrega = entrega.slice(10)
-        entrega = parseFloat(entrega.replace(',','.').replace(' ',''))
-        total = entrega+price
-      }else{
+        entrega = parseFloat(entrega.replace(",", ".").replace(" ", ""))
+        total = entrega + price
+      } else {
         total = price
       }
 
       const logo = await page.$eval(
-        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > img`, imagem =>imagem.src
+        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > img`,
+        imagem => imagem.src
       )
-     /* const nome = await page.$eval(
+      /* const nome = await page.$eval(
         `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > img`, imagem =>imagem.alt
       )
       */
-      
+
       const img = await page.$eval(
-        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > div.dish-card__container-image > img`, imagem =>imagem.src
+        `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > div.dish-card__container-image > img`,
+        imagem => imagem.src
       )
-      var status = ''
-  
-      if ((await page.$(`#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > span`)) !== null) {
-        
+      var status = ""
+
+      if (
+        (await page.$(
+          `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > span`
+        )) !== null
+      ) {
         status = await page.$eval(
-          `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > span`, ver => ver.innerText
+          `#marmita-panel1-3 > div > ul > li:nth-child(${i}) > div > a > span`,
+          ver => ver.innerText
         )
-      } 
+      }
       total.toFixed(2)
-  
+
       //arrayProducts.push({link:link,titulo:title,desc:description,nome:nome,preco:price,tempo:tempo,entrega:entrega,logo:logo,img:img,status:status,total:total})
-      arrayProducts.push({link:link,titulo:title,desc:description,tempo:tempo,logo:logo,img:img,status:status,total:total})
+      arrayProducts.push({
+        link: link,
+        titulo: title,
+        desc: description,
+        tempo: tempo,
+        logo: logo,
+        img: img,
+        status: status,
+        total: total,
+      })
     }
     context.close()
 
     res.send(arrayProducts)
     console.log(arrayProducts)
     return arrayProducts
-
-  }catch(err){
+  } catch (err) {
     context.close()
     console.log(err)
-    res.send('Algo deu errado, confirme se você digitou corretamente o endereço e a comida desejada',err)
+    res.send(
+      "Algo deu errado, confirme se você digitou corretamente o endereço e a comida desejada",
+      err
+    )
   }
- 
-
-
-}
-)
+})
--- iPrice/src/App.js
+++ iPrice/src/App.js
@@ -1,16 +1,16 @@
-import React, {useState} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
+import React, { useState } from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import Grid from "@material-ui/core/Grid"
 import SyncLoader from "react-spinners/SyncLoader"
-import './App.css';
-import Api from './api/api'
+import "./App.css"
+import Api from "./api/api"
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    width: '100%',
-    display: 'flex',
-    flexWrap:"wrap",
+    width: "100%",
+    display: "flex",
+    flexWrap: "wrap",
   },
   paper: {
     height: 140,
@@ -19,153 +19,152 @@ const useStyles = makeStyles((theme) => ({
   control: {
     padding: theme.spacing(2),
   },
-}));
+}))
 
 const headers = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers":
+    "Origin, X-Requested-With, Content-Type, Accept",
 }
 function App() {
-  const [values,setValues] = useState('')
-  const [spinner,setSpinner] = useState(false)
+  const [values, setValues] = useState("")
+  const [spinner, setSpinner] = useState(false)
   const [respostas, setRespostas] = useState([])
-  const classes = useStyles();
+  const classes = useStyles()
 
-  const raiseInvoiceClicked = (link)=>{
-    
-    window.open(link, "_blank") 
- }
-  
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-    console.log(event.target.value)
-  };
-
-  const pedeComida = async({endereco,comida,numero,cidade})=>{
-    endereco = endereco+numero+cidade
-    console.log(endereco)
-     try{ setSpinner(true)
-      setRespostas([])
-      const response = await Api.post('/foods',{endereco,comida},{headers:headers})
-      setRespostas(response.data.sort((a, b) => (a.total > b.total) ? 1 : -1))
-    }
-    catch(err){
-        setSpinner(false)
-        alert('Algo deu errado, reveja seu endereço e comida')
-      }
-      
+  const raiseInvoiceClicked = link => {
+    window.open(link, "_blank")
   }
 
+  const handleChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value })
+    console.log(event.target.value)
+  }
+
+  const pedeComida = async ({ endereco, comida, numero, cidade }) => {
+    endereco = endereco + numero + cidade
+    console.log(endereco)
+    try {
+      setSpinner(true)
+      setRespostas([])
+      const response = await Api.post(
+        "/foods",
+        { endereco, comida },
+        { headers: headers }
+      )
+      setRespostas(response.data.sort((a, b) => (a.total > b.total ? 1 : -1)))
+    } catch (err) {
+      setSpinner(false)
+      // alert("Algo deu errado, reveja seu endereço e comida")
+    }
+  }
 
   return (
     <div>
-      
-  <div 
+      <div className="App">
+        <div>
+          <h1 style={{ paddingLeft: 10 }}>
+            iPrice o melhor preço para sua refeição
+          </h1>
+        </div>
 
-  className="App">
-    <div
-    
-    >
-      <h1 style={{paddingLeft:10}}>iPrice o melhor preço para sua refeição</h1>
-    </div>
-    
-  <h4 style={{paddingLeft:10}}> O tempo médio para a resposta da pesquisa é de 30s</h4>
-      <div>
-        
-      <h3>Rua</h3>
-                <input  
-                onChange={handleChange('endereco')}
-                value={values.endereco}
-                className='pesquisa'
-                placeholder='Ex: Av brasil'
-                /> <h3>Número</h3> <input  
-                onChange={handleChange('numero')}
-                value={values.numero}
-                className='pesquisa'
-                placeholder='Ex: 000'
-                />
-                  <h3>Cidade</h3> <input  
-                onChange={handleChange('cidade')}
-                value={values.cidade}
-                className='pesquisa'
-                placeholder='Ex: São Paulo'
-                />
-            <h3>Comida</h3>
-            <input 
-             onChange={handleChange('comida')}
-             value={values.comida}
-            className='pesquisa'
-            placeholder='Ex: Pizza'
-            />
-            
+        <h4 style={{ paddingLeft: 10 }}>
+          {" "}
+          O tempo médio para a resposta da pesquisa é de 30s
+        </h4>
+        <div>
+          <h3>Rua</h3>
+          <input
+            onChange={handleChange("endereco")}
+            value={values.endereco}
+            className="pesquisa"
+            placeholder="Ex: Av brasil"
+          />{" "}
+          <h3>Número</h3>{" "}
+          <input
+            onChange={handleChange("numero")}
+            value={values.numero}
+            className="pesquisa"
+            placeholder="Ex: 000"
+          />
+          <h3>Cidade</h3>{" "}
+          <input
+            onChange={handleChange("cidade")}
+            value={values.cidade}
+            className="pesquisa"
+            placeholder="Ex: São Paulo"
+          />
+          <h3>Comida</h3>
+          <input
+            onChange={handleChange("comida")}
+            value={values.comida}
+            className="pesquisa"
+            placeholder="Ex: Pizza"
+          />
+        </div>
+
+        <button onClick={() => pedeComida(values)} className="botao">
+          Pesquisar
+        </button>
       </div>
-           
-            <button onClick={()=>pedeComida(values)} className='botao'>
-              Pesquisar
-            </button>
-              
-          </div>
+      <div>
+        {respostas.length > 1 ? (
           <div>
-            {respostas.length > 1? <div>
             <Grid container className={classes.root} spacing={2}>
-            <Grid item xs={12}>
-              <Grid container justify="center" spacing={2}>
-                {respostas.map((comida,i) => (
-                  <Grid key={i} className='grid' item>
-                    <div
-                    className='allCard'
-                
-                    >
-                      <div className='image'>
-                        <img style={{
-                              width: '100%',
-                              height: '100%',
-                        }}src={comida.img}/>
-                        {comida.status!=='Fechado'? null:<span class='spanStatus'>{comida.status}</span>}
+              <Grid item xs={12}>
+                <Grid container justify="center" spacing={2}>
+                  {respostas.map((comida, i) => (
+                    <Grid key={i} className="grid" item>
+                      <div className="allCard">
+                        <div className="image">
+                          <img
+                            style={{
+                              width: "100%",
+                              height: "100%",
+                            }}
+                            src={comida.img}
+                          />
+                          {comida.status !== "Fechado" ? null : (
+                            <span class="spanStatus">{comida.status}</span>
+                          )}
+                        </div>
+
+                        <img className="logo" src={comida.logo} />
+                        <div className="info">
+                          <div className="topContent">
+                            <h3 className="title">{comida.titulo}</h3>
+                            <span className="desc">{comida.desc}</span>
+                          </div>
+
+                          <span className="price">
+                            {comida.tempo} Total: R${comida.total}{" "}
+                          </span>
+                          <button
+                            className="botao cardbotao"
+                            onClick={() => raiseInvoiceClicked(comida.link)}>
+                            Faça seu pedido
+                          </button>
+                        </div>
                       </div>
-                      
-                      <img className='logo' src={comida.logo}/>
-                      <div className='info'>
-                        <div className='topContent'>
-                          <h3 className='title'>{comida.titulo}</h3>
-                          <span className='desc'>{comida.desc}</span>
-                         </div>
-              
-                           <span className='price'>{comida.tempo} Total: R${comida.total} </span>
-                           <button className='botao cardbotao' onClick={()=>raiseInvoiceClicked(comida.link)} >Faça seu pedido</button>
-                         
-                         
-                      </div>
-                     
-                    </div>
-                  </Grid>
-                ))}
+                    </Grid>
+                  ))}
+                </Grid>
               </Grid>
             </Grid>
-            </Grid>
           </div>
-       :
-       <div
-       style={{
-         display:'flex',
-         justifyContent:'center',
-         alignItems:'center',
-       }}
-       >
-            <SyncLoader
-
-      size={15}
-      color={"#c93e1f"}
-      loading={spinner}
-      />
+        ) : (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}>
+            <SyncLoader size={15} color={"#c93e1f"} loading={spinner} />
+          </div>
+        )}
       </div>
-        }
-            </div>
-         
-         
     </div>
-    
+  )
+}
 
-    );}
-
-export default App;
+export default App
